985fd71a682c04cd98686353477e422224bfd118
y0584 <yuanm@dtdream.com>
2021-01-24 20:43:49 +0800

    ejs render

---
+++ demo/02.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = new express();
+const PORT = 3000;
+
+// 指定path路径&渲染模板
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+  const item = ['item1','item2','item3','item4','item5'];
+  res.render('index',{item});
+});
+
+app.listen(PORT,()=>{
+   console.log(`server listening port ${PORT}`);
+})
