322aa72c921c1ea410e132b26dc66449a99bccbc
y0584 <yuanm@dtdream.com>
2021-01-28 20:15:57 +0800

    优化

--- demo/04.js
+++ demo/04.js
@@ -6,20 +6,29 @@ const express = require('express');
 const mongodb = require('mongodb').MongoClient;
 
 const app = express();
-const URL = 'mongodb://127.0.0.1:27017';
+const URL = 'mongodb://127.0.0.1:27017/';
 const DBNAME = 'tongtang';
 
 app.get('/',(req,res)=>{
+  // 一 3.0 之前的方式
+  // mongodb.connect(URL, (err, db)=>{
+  //   const result = db.collection('user').find();
+  //   result.toArray((err,data)=>{
+  //     console.log('result_data:',data);
+  //   });
+  //   db.close();
+  // });
+
+  // 二 3.0 后的方式
   mongodb.connect(URL, (err, client)=>{
     const table = client.db(DBNAME);
-    const result = table.collection('user').find();
-    // console.log('db result:',result);
+    const result = table.collection('user').find({'name':'hualingfeng'});
     result.toArray((err,data)=>{
       console.log('result_data:',data);
     });
     client.close();
-    res.send('hellow mongodb<br/>');
   });
+  res.send('hellow mongodb<br/>');
 });
 
 app.listen(3000,()=>{
