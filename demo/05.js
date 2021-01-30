b91f4904388cc80c4bab9afd4463bd28d25b599e
y0584 <yuanm@dtdream.com>
2021-01-30 23:43:56 +0800

    update mongodb

--- demo/05.js
+++ demo/05.js
@@ -4,18 +4,46 @@
 const { json } = require('body-parser');
 const express = require('express');
 const DB = require('./db');
+const DB2 = require('./db/index_es5');
 
 const app = express();
 
-app.get('/',async(req,res)=>{
-  // 大于22
-  const userInfo = await DB.find('user', {'age': {$gt:22}})
-  // 等于
-  // const userInfo = await DB.find('user', {'name': 'hualingfeng'})
-  console.log('userInfo',userInfo);
-  res.send('user information:<br/>'+ JSON.stringify(userInfo))
+app.get('/', (req, res) => {
+  res.redirect('/es6');
 });
 
-app.listen(3000,()=>{
- console.log('listening port on 3000');
+app.get('/es6', async (req, res) => {
+  const userInfo = await DB.find('user', { 'age': { $gt: 22 } });  // 大于22
+  // const userInfo = await DB.find('user', {'name': 'hualingfeng'}); // 等于
+  console.log('userInfo', userInfo);
+  res.send('user information:<br/>' + JSON.stringify(userInfo))
+});
+
+app.get('/es5', (req, res) => {
+  DB2.find('user',{'name':'hualingfeng'},(err,data)=>{
+    if(err){
+      console.log('find err')
+      res.send('find err:<br/>')
+    }else{
+      console.log('all userInfo', data);
+      res.send('all user information:<br/>' + JSON.stringify(data))
+    }
+  })
+});
+
+app.get('/es5update', (req, res) => {
+  DB2.update('user',{'name':'hualingfeng'},{'pwd':'123456'},(err,data)=>{
+    if(err){
+      console.log('find err')
+      res.send('update err:<br/>')
+    }else{
+      console.log('all userInfo', data);
+      res.send('update success:<br/>')
+    }
+  })
+});
+
+
+app.listen(3000, () => {
+  console.log('listening port on 3000');
 });
--- demo/db/index_es5.js
+++ demo/db/index_es5.js
@@ -1,7 +1,52 @@
 /**
- * mongodb crud es5封装
+ * mongodb crud callback封装
  */
 const MongoClient = require('mongodb').MongoClient;
 const URL = 'mongodb://127.0.0.1:27017/';
 const DBNAME = 'tongtang';
+
+/**
+ * 连接数据库
+ * @param {String} collectionName 回调函数
+ */
+const dbConnect = (callback) =>{
+  MongoClient.connect(URL,(err,client)=>{
+    if(err){
+      console.log('mongodb connect err');
+      return;
+    }
+    const db = client.db(DBNAME);
+    callback(db);
+    client.close();
+  })
+}
+
+/**
+ * 查找
+ * @param {string} collectionName 表名
+ * @param {*} json 查找条件
+ */
+exports.find = (collectionName, json, callback)=>{
+  dbConnect((db)=>{
+    const collection = db.collection(collectionName);
+    collection.find(json).toArray((err,data)=>{
+      callback(err,data);
+    })
+  })
+}
+
+/**
+ * 修改
+ * @param {string} collectionName 表名
+ * @param {*} json1 查找条件
+ * @param {*} json1 更新数据
+ */
+exports.update = (collectionName, json1, json2, callback)=>{
+  dbConnect((db)=>{
+    const collection = db.collection(collectionName);
+    collection.updateOne(json1, { $set: json2 }, (err,result)=>{
+      callback(err,result);
+    })
+  })
+}
 
