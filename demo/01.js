9810b28f2f9d45baa4d50a606db0786ac7bca8dd
y0584 <tc_hualingfeng@163.com>
2021-02-09 15:19:16 +0800

    注释修改

--- demo/01.js
+++ demo/01.js
@@ -14,14 +14,17 @@ app.get('/',(req,res)=>{
  res.send('hellow express');
 })
 
+// 动态路由
 app.get('/news/:aid',(req,res)=>{
   const { aid } = req.params
+  console.log('req.params:',req.params);
   res.send('news动态路由:'+aid);
 })
 
+// get 路由
 app.get('/list',(req,res)=>{
   const { type } = req.query;
-   res.send('list get路由:'+type);
+  res.send('list get路由:'+type);
 })
 
 app.listen(PORT,()=>{
